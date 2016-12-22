6632fbea39a001d61b0772d49e61504a7874a487
Harry Lachenmayer <harrylachenmayer@gmail.com>
2016-12-22 18:07:57 +0000

    add ga

--- shell.js
+++ shell.js
@@ -18,6 +18,14 @@ const shell = `
 <body>
 <div id="container"></div>
 <script src="/static/app.js"></script>
+<script>
+(function(s,p,y,w,a,r,e){s['GoogleAnalyticsObject']=a;s[a]=s[a]||function(){
+(s[a].q=s[a].q||[]).push(arguments)},s[a].l=1*new Date();r=p.createElement(y),
+e=p.getElementsByTagName(y)[0];r.async=1;r.src=w;e.parentNode.insertBefore(r,e)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-88923602-2', 'auto');
+ga('send', 'pageview');
+</script>
 </body>
 </html>
 `
