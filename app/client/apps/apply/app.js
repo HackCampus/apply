50ed78c553a385d8de978ee8a8b193edd2aceb7d
Harry Lachenmayer <harrylachenmayer@gmail.com>
2017-02-06 15:14:37 +0000

    add late warning

--- app/client/apps/apply/app.js
+++ app/client/apps/apply/app.js
@@ -232,6 +232,7 @@ module.exports = Component({
       <div class="apply">
         <h1>Apply to HackCampus</h1>
         <p>Welcome! This is the application form for the <a href="http://hackcampus.io/">HackCampus internship programme</a>. If something doesn't work, we'd appreciate it if you <a href="https://github.com/hackcampus/apply/issues">file an issue on GitHub</a> or email us at contact@hackcampus.io. Thanks! :)</p>
+        <p><strong>The application deadline for the ${constants.programmeYear} programme was on the ${constants.applicationDeadline}. You can still apply, but we can not guarantee that you will be considered for this year's interviews.</strong></p>
         ${section('step0', 'Step 0: Authenticate', children.authenticate())}
         ${section('step1', 'Step 1: Personal details', user ? children.personalDetails(extend(props, {connect})) : '')}
         ${section('step2', 'Step 2: Tech preferences', user ? children.techPreferences(props) : '')}
